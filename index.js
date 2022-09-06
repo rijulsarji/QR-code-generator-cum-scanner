0657e8203e74d9add47dd64199f72b25186135e7
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-09-06 23:01:27 +0530

    estabilished connection

--- index.js
+++ index.js
@@ -4,5 +4,18 @@ const mongoose = require("mongoose");
 const app = express(); 
 app.use(express.json())
 
-const dbUri = process.env.PORT
-console.log(dbUri)
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening to ${port}...`)
+})
+
+// connecting to database
+const dbURi = process.env.dbURi;
+
+mongoose
+  .connect(dbURi)
+  .then(() => {
+    console.log("Connected to database...")
+  })
+  .catch(err => console.log(err))
